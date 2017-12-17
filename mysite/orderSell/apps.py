c285d1738d4bc8f52bbe1429d2a3d1c49281d28b
Unknown <ange.crespo@hotmail.fr>
2017-12-17 08:26:52 +0100

    Prepare the Template dispatch
    Soon the template will be fully iplemented the function will have to be feed (sometimes with AJAX method):)

---
+++ mysite/orderSell/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OrdersellConfig(AppConfig):
+    name = 'orderSell'
---
+++ mysite/orderTrade/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OrdertradeConfig(AppConfig):
+    name = 'orderTrade'
