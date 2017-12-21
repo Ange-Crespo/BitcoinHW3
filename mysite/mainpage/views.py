531ebdd1bb3b5867489ea5bd9ce510ae28220ad0
JerryShen <jerry82422@gmail.com>
2017-12-21 18:26:45 +0800

    Shortcircuit the authenticate

--- mysite/mainpage/views.py
+++ mysite/mainpage/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponseRedirect
 # Create your views here.
 
 def index(request):
-    if request.user.is_authenticated() is False:
-        return HttpResponseRedirect('/user/login')
+    # if request.user.is_authenticated() is False:
+    #     return HttpResponseRedirect('/user/login')
     return render(request, 'mainpage/index.html')
