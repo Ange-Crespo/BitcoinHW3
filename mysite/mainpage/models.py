8be42e77e0e7ac04df2042dae261e5881bacb5a7
JerryShen <jerry82422@gmail.com>
2017-12-22 20:04:51 +0800

    Add `currency` model

--- mysite/mainpage/models.py
+++ mysite/mainpage/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class currency(models.Model):
+    symbol = models.CharField(max_length = 5)
+    name = models.CharField(max_length = 10)
+    icon_absolute_link = models.CharField(max_length = 255)
+    icon_relative_link = models.CharField(max_length = 255)
