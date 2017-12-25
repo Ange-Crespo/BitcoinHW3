3ab2c365cde4ee61d75ec2e40f994bc20740c036
YAN-BING SONG <r05944043@ntu.edu.tw>
2017-12-25 21:03:12 +0800

    icon

--- mysite/mainpage/views.py
+++ mysite/mainpage/views.py
@@ -4,6 +4,11 @@ from django.http import HttpResponseRedirect
 # Create your views here.
 
 def index(request):
+<<<<<<< Updated upstream
     if request.user.is_authenticated() is False:
         return HttpResponseRedirect('/user/login')
+=======
+    #if request.user.is_authenticated() is False:
+    #    return HttpResponseRedirect('/user/login')
+>>>>>>> Stashed changes
     return render(request, 'mainpage/index.html')
