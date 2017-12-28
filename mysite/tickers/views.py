eaf5709c39b5d3e25c5f54c55b91ccd884ab5d51
JerryShen <jerry82422@gmail.com>
2017-12-28 10:48:40 +0800

    Use `history.json` as data source for `tickers`

--- mysite/tickers/views.py
+++ mysite/tickers/views.py
@@ -1,30 +1,22 @@
 import json
 from django.http import HttpResponse
 from django.shortcuts import render
+from functools import reduce
 
 # Create your views here.
 
 def index(request):
+    json_data = open('../json/history.json')
+    data = json.load(json_data)
+    first_price = float(data[0].get('bidprice'))
+    price = float(data[-1].get('bidprice'))
+    volume = reduce((lambda x, y: x + float(y.get('amount'))), data, 0)
+    change = float((price - first_price) / first_price)
     result = {
         "code": 200,
         "data": [
             {
-                'symbol': "btc", 'last': 323, 'percent': 53, 'volume': 600
-            },
-            {
-                'symbol': "eth", 'last': 345, 'percent': 34, 'volume': 567
-            },
-            {
-                'symbol': "ltc", 'last': 567, 'percent': 67, 'volume': 456
-            },
-            {
-                'symbol': "dash", 'last': 234, 'percent': 23, 'volume': 890
-            },
-            {
-                'symbol': "dash", 'last': 234, 'percent': 23, 'volume': 890
-            },
-            {
-                'symbol': "zec", 'last': 111, 'percent': 78, 'volume': 345
+                'symbol': "btc", 'price': price, 'change': change, 'volume': volume
             }
         ]
     }
